343957316bb6a8630ed51ae97ba4cb3477b17b90
web-wj940622 <mac@macdeMacBook-Air-6.local>
2016-06-30 16:05:31 -0700

    图形旋转

---
+++ webstorm/06-28/RegPoint/main.js
@@ -0,0 +1,37 @@
+/**
+ * Created by mac on 16/6/28.
+ */
+(function () {
+    
+    function Main() {
+        this._context2d = document.getElementById('canvas').getContext('2d');
+
+        this._infoDiv = document.getElementById('info');
+        this._currentTime = 0;
+
+        this._r = new ucai.Rect(100,100);
+        // this._r.rotation = Math.PI/4;
+        this._r.x =100;
+        this._r.y = 100;
+        this._r.regX = 50;
+        this._r.regY = 50;
+        // this._r.draw(this._context2d);
+
+        this.render(this._currentTime);
+    }
+
+    Main.prototype.render = function (time) {
+        var deltaTime = time - this._currentTime;
+        this._infoDiv.innerHTML = Math.round(1000/deltaTime) + "fps";
+        this._currentTime = time;
+
+        //clear canvas
+        this._context2d.clearRect(0,0,550,400);
+        this._r.rotation += .01;
+        this._r.draw(this._context2d);
+        requestAnimationFrame(this.render.bind(this));
+    };
+
+
+    new Main();
+})();
