4d5d7ba3a0f173bb4423c67dc4d53a729577d584
wjwalker <664340431@qq.com>
2017-05-17 18:09:06 +0800

    京东无延迟菜单

---
+++ css/js京东无延迟菜单/js/function.js
@@ -0,0 +1,44 @@
+/**
+ * Created by wjwj12345 on 5/15/2017.
+ */
+function sameSign(a, b) {
+    return (a ^ b) >= 0
+}
+
+function vector(a, b) {
+    return {
+        x: b.x - a.x,
+        y: b.y - a.y
+    }
+}
+
+function vectorProduct(v1, v2) {
+    return v1.x * v2.y - v1.y * v2.x
+}
+
+function isPointInTrangle(p, a, b, c) {
+    var pa = vector(p, a);
+    var pb = vector(p, b);
+    var pc = vector(p, c);
+
+    var t1 = vectorProduct(pa, pb);
+    var t2 = vectorProduct(pb, pc);
+    var t3 = vectorProduct(pc, pa);
+
+    return sameSign(t1, t2) && sameSign(t2, t3)
+}
+
+function needDelay(elem, leftCorner, currMouserPos) {
+    var offset = elem.offset();
+    var topLeft = {
+        x: offset.left,
+        y: offset.top
+    };
+
+    var bottomLeft = {
+        x: offset.left,
+        y: offset.top + elem.height()
+    };
+
+    return isPointInTrangle(currMouserPos, leftCorner, topLeft, bottomLeft)
+}
