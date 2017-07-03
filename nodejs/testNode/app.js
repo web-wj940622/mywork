c786ce10159d7e37dc62c2ace10c90aa3c6c2d81
wjwalker <664340431@qq.com>
2017-07-03 16:53:43 +0800

    basic-auth认证

---
+++ nodejs/testNode/app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var basicAuth = require('basic-auth');
+var auth = function (req, resp, next) {
+    function unauthorized(resp) {
+        resp.set('WWW-Authenticate', 'Basic realm=Input User&Password');
+        return resp.sendStatus(401);
+    }
+    var user = basicAuth(req);
+    if (!user || !user.name || !user.pass) {
+        return unauthorized(resp);
+    }
+    if (user.name === 'User' && user.pass === 'Password') {
+        return next();
+    } else {
+        return unauthorized(resp);
+    }
+};
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+app.get('/auth', auth, function(req, resp) {
+    resp.status(200).send('Authorization');
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
